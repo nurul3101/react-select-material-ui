547e3d17011e4dbd7ff4a45a2643cea0061c67de
Nurul Sundarani <nurulraza26@gmail.com>
2018-06-25 19:40:11 +0530

    Resolved material issue
    Resolved MuiTheme Issue

--- src/index.js
+++ src/index.js
@@ -1,63 +1,50 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import ChipInput from "material-ui-chip-input";
-import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ChipInput from 'material-ui-chip-input'
+import { MuiThemeProvider, createMuiTheme } from 'material-ui'
 
-import "./styles.css";
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      // light: will be calculated from palette.primary.main,
-      main: "#0391EA"
-      // dark: will be calculated from palette.primary.main,
-      // contrastText: will be calculated to contast with palette.primary.main
-    }
-
-    // error: will use the default color
-  }
-});
+import './styles.css'
 
 class App extends React.Component {
   state = {
     chipsValue: []
-  };
+  }
 
   render() {
-    const dataSource = ["Sofa", "Chairs", "Tables"];
+    const dataSource = ['Sofa', 'Chairs', 'Tables']
 
     const handleAddChip = receivedChip => {
-      console.log("I was called with chip", receivedChip);
-      const newChips = [...this.state.chipsValue];
-      newChips.push(receivedChip);
+      console.log('I was called with chip', receivedChip)
+      const newChips = [...this.state.chipsValue]
+      newChips.push(receivedChip)
       this.setState(
         {
           chipsValue: newChips
         },
-        () => console.log("State after Adding", this.state.chipsValue)
-      );
-    };
+        () => console.log('State after Adding', this.state.chipsValue)
+      )
+    }
 
     const handleDeleteChip = (chipToBeDeleted, index) => {
-      console.log(index);
-      const newChips = [...this.state.chipsValue];
-      newChips.splice(index, 1);
+      console.log(index)
+      const newChips = [...this.state.chipsValue]
+      newChips.splice(index, 1)
       this.setState(
         {
           chipsValue: newChips
         },
-        () => console.log("State after Removing", this.state.chipsValue)
-      );
-    };
+        () => console.log('State after Removing', this.state.chipsValue)
+      )
+    }
 
     const beforeAdd = chip => {
-      console.log("chip value", chip);
-      console.log(dataSource.includes(chip));
-      return dataSource.includes(chip);
-    };
+      console.log('chip value', chip)
+      console.log(dataSource.includes(chip))
+      return dataSource.includes(chip)
+    }
 
     return (
-      <MuiThemeProvider theme={theme}>
+      <MuiThemeProvider>
         <ChipInput
           value={this.state.chipsValue}
           fullWidth={true}
@@ -71,9 +58,9 @@ class App extends React.Component {
           onBeforeRequestAdd={chip => beforeAdd(chip)}
         />
       </MuiThemeProvider>
-    );
+    )
   }
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
